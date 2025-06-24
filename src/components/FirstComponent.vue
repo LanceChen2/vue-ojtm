2129fb3bf758337746a78bca6d0f99e5425dcfad
Lance <jchenlance@gmail.com>
2025-06-24 21:12:03 +0800

    remove default components, and add 1st & 2nd myself

---
+++ src/components/FirstComponent.vue
@@ -0,0 +1,12 @@
+<template>
+    <h2>第一個 Component</h2>
+</template>
+
+<style scoped>
+h2{
+    color: green;
+}
+</style>
+
+<script setup>
+</script>
---
+++ src/components/SecondComponent.vue
@@ -0,0 +1,27 @@
+<!-- <template>
+    <h2>第二個 Component</h2>
+</template>
+
+<style scoped>
+h2{
+    color: blue;
+}
+</style>
+
+<script setup>
+</script> -->
+ <script setup>
+    
+ </script>
+ 
+ <template>
+    <div>
+        <h2>第二個 Component</h2>
+    </div>
+ </template>
+ 
+ <style lang="css" scoped>
+    h2{
+        color: blue;
+    }
+ </style>
