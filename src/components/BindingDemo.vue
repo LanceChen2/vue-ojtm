2d232b4b57d9bc826d9d7dd4320937752ce2a233
Lance <jchenlance@gmail.com>
2025-06-28 10:05:08 +0800

    static files of photos shown in web

--- src/components/BindingDemo.vue
+++ src/components/BindingDemo.vue
@@ -1,9 +1,13 @@
 <script setup>
 import { ref } from 'vue';
+import myCat from '@/assets/images/cat3.jpeg'; 
 
 //tab enter
 const userName = ref('Tom');
 const isAdmin = ref(false);
+
+const dog = ref('/images/dog1.jpeg');
+const cat = ref('/src/assets/images/cat3.jpeg');
 </script>
 
 <template>
@@ -15,8 +19,17 @@ const isAdmin = ref(false);
         <!-- 如果isAdmin為true就顯示管理者登入，如果是false就不顯示 -->
         <p>{{ isAdmin ? '管理者登入' : '' }}</p>
         <!-- <p>{{ isAdmin && '管理者登入'}}</p> -->
+        <img src="/images/dog1.jpeg" />
+        <img :src="dog" /> 
+        <img src="@/assets/images/cat3.jpeg" title="不建議這樣用" />
+        <img :src="cat" title="不建議這樣用" /> 
+        <img :src="myCat" title="建議這樣用" />
 
     </div>
 </template>
 
-<style lang="css" scoped></style>
+<style lang="css" scoped>
+img{
+    width: 100px;
+}
+</style>
