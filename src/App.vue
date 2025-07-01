52446ce2831cbed809c7169d26638bc147c1b9e5
Lance <jchenlance@gmail.com>
2025-07-01 21:01:17 +0800

    complete App.vue templating

--- src/App.vue
+++ src/App.vue
@@ -1,4 +1,7 @@
 <script setup>
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap/dist/js/bootstrap.js'
+import 'bootstrap-icons/font/bootstrap-icons.css'
 import ApiStyle from './components/ApiStyle.vue';
 import BindingComponent from './components/BindingComponent.vue';
 import BindingDemo from './components/BindingDemo.vue';
@@ -14,23 +17,27 @@ import WeekDay from './components/WeekDay.vue';
 import AboutView from './views/AboutView.vue';
 import ContactView from './views/ContactView.vue';
 import HomeView from './views/HomeView.vue';
+import HeaderComponent from './components/HeaderComponent.vue';
+import JumbotronComponent from './components/JumbotronComponent.vue';
+import FooterComponent from './components/FooterComponent.vue';
 
 </script>
 
 <template>
-    <h2 id="apph2">Hello, Vue!!</h2>
+    <HeaderComponent></HeaderComponent>
+    <JumbotronComponent></JumbotronComponent>
+    <div class="container mt-3">
+    <RouterView></RouterView>
+    <!-- <h2 id="apph2">Hello, Vue!!</h2> -->
     <!-- <RouterLink to="/">首頁</RouterLink> |
     <RouterLink to="/about_ispan">關於我們</RouterLink> |
     <RouterLink to="/contact">聯絡我們</RouterLink> | -->
-    <RouterLink :to="{name: 'home'} " activeClass="active">首頁</RouterLink> |
+    <!-- <RouterLink :to="{name: 'home'} " activeClass="active">首頁</RouterLink> |
     <RouterLink :to="{name: 'about'}" activeClass="active">關於我們</RouterLink> |
     <RouterLink :to="{name: 'contact'}" activeClass="active">聯絡我們</RouterLink> |
     <RouterLink :to="{name: 'member', params: {id:'10'}}" activeClass="active">會員中心</RouterLink> |
-    <RouterLink :to="{name: 'todos' }" activeClass="active">待做事項</RouterLink> |
+    <RouterLink :to="{name: 'todos' }" activeClass="active">待做事項</RouterLink> | -->
 
-
-    <hr>
-    <RouterView />
     <!-- <HomeView />
     <AboutView /> 
     <ContactView /> -->
@@ -48,6 +55,8 @@ import HomeView from './views/HomeView.vue';
     <HasBook />
     <hr>
     <BindingDemo />  -->
+    </div>
+    <FooterComponent></FooterComponent>
 </template>
 
 <style scoped>
